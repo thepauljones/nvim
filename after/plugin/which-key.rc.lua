dbc876d016faa3d5694146da0e2acde15e734bb6
Paul Jones <thepauljones@gmail.com>
2022-11-04 08:39:41 +0100

    Added diagnostics to which key mappings (lspsaga)

--- after/plugin/which-key.rc.lua
+++ after/plugin/which-key.rc.lua
@@ -2,27 +2,27 @@
 local wk = require("which-key")
 
 wk.register({
-  ["p"] = {
-    name = "Packer",
-    c = { "<cmd>PackerCompile<cr>", "Compile" },
-    i = { "<cmd>PackerInstall<cr>", "Install" },
-    s = { "<cmd>PackerSync<cr>", "Sync" },
-    S = { "<cmd>PackerStatus<cr>", "Status" },
-    u = { "<cmd>PackerUpdate<cr>", "Update" },
-  },
+    ["p"] = {
+        name = "Packer",
+        c = { "<cmd>PackerCompile<cr>", "Compile" },
+        i = { "<cmd>PackerInstall<cr>", "Install" },
+        s = { "<cmd>PackerSync<cr>", "Sync" },
+        S = { "<cmd>PackerStatus<cr>", "Status" },
+        u = { "<cmd>PackerUpdate<cr>", "Update" },
+    },
 }, { prefix = "<leader>" })
 
 -- setup which key group mapping for "l"
 wk.register({
-  ["l"] = {
-    name = "Lspsaga",
-    a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
-    d = { "<cmd>Lspsaga preview_definition<cr>", "Preview Definition" },
-    h = { "<cmd>Lspsaga hover_doc<cr>", "Hover Doc" },
-    r = { "<cmd>Lspsaga rename<cr>", "Rename" },
-    f = { "<cmd>Lspsaga lsp_finder<cr>", "Lsp Finder" },
-    ca = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
-    cc = { "<cmd>Lspsaga show_cursor_diagnostics<cr>", "Show Cursor Diagnostics" },
-    cd = { "<cmd>Lspsaga show_line_diagnostics<cr>", "Show Line Diagnostics" },
-  },
+    ["l"] = {
+        name = "Lspsaga",
+        a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
+        d = { "<cmd>Lspsaga preview_definition<cr>", "Preview Definition" },
+        h = { "<cmd>Lspsaga hover_doc<cr>", "Hover Doc" },
+        r = { "<cmd>Lspsaga rename<cr>", "Rename" },
+        f = { "<cmd>Lspsaga lsp_finder<cr>", "Lsp Finder" },
+        ca = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
+        cc = { "<cmd>Lspsaga show_cursor_diagnostics<cr>", "Show Cursor Diagnostics" },
+        cd = { "<cmd>Lspsaga show_line_diagnostics<cr>", "Show Line Diagnostics" },
+    },
 }, { prefix = "<leader>" })
